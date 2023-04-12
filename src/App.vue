6e8311bd2cfe867704cd33a9a052104b4e6cc547
Elzodxon Sharofaddinov <elzodxonsharofaddinov@Elzodxons-MacBook-Pro.local>
2023-04-12 14:28:17 +0500

    feat: welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="bg-[#141415] h-screen text-white p-10">
-    <p class="mb-5">
-      <a href="https://uic.group" target="_blank">
-        <img src="https://uic.group/img/logo/logo.svg" alt="UIC Group" />
-      </a>
-    </p>
-    <p>
-      Blank template, write your code here.
-      <a
-        class="underline"
-        href="https://github.com/UIC-Group/vue3-powerbuild"
-        target="_blank"
-      >
-        GitHub repo
-      </a>
-    </p>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full sm:w-96">
+      <h1 class="text-3xl font-bold mb-6 text-center">Welcome</h1>
+      <p class="text-gray-600 text-center">Thank you for using our internal boilerplate!</p>
+      <div class="mt-6">
+        <a href="https://uic.group" target="_blank" class="block bg-blue-500 text-white rounded-lg px-4 py-2 mx-auto text-center w-full">
+          Learn more
+        </a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+}
+</script>
+
+<style scoped>
+/* Add any additional custom styles here */
+</style>
