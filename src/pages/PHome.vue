ab6955406e9f1eb7c14c7521584e8ee2ed8f3dd4
Elzodxon Sharofaddinov <elzodxonsharofaddinov@Elzodxons-MacBook-Pro.local>
2023-04-12 15:39:43 +0500

    feat: splash screen

--- src/pages/PHome.vue
+++ src/pages/PHome.vue
@@ -1,10 +1,20 @@
 <template>
-  <WelcomePage />
+  <div>
+    <SplashScreen v-if="loading" />
+    <div v-else>
+      <WelcomePage />
+    </div>
+  </div>
 </template>
-<script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+import SplashScreen from "@/components/SplashScreen.vue";
 import WelcomePage from "@/components/WelcomePage.vue";
 
-export default {
-  components: { WelcomePage },
-};
+const loading = ref(true);
+
+setTimeout(() => {
+  loading.value = false;
+}, 1000);
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#111111",
+          100: "#060C13",
+        },
+        gray: {
+          DEFAULT: "#79899B",
+          100: "#EDF1F5",
+        },
+        blue: {
+          DEFAULT: "#008FF6",
+        },
+      },
+    },
   },
   plugins: [],
 };
