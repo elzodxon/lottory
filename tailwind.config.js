ddf3db55ca5d22614f148f8220ada1dd0a17b640
Ahrorbek Uralbayev <ahrorbek.uralbayev1@gmail.com>
2023-09-22 11:43:41 +0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
         green: {
           DEFAULT: "#63D455",
         },
+        creamy:{
+          DEFAULT: "#DFA290"
+    },
         darkCream:{
           DEFAULT: "#5B432C"
         }
